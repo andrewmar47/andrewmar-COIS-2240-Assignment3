c131cc7f4681be75075271f94d9316202bbab529
Andrew Mar <andrewmar@trentu.ca>
2025-04-01 21:09:25 -0400

    Add VehicleRentalTest JUnit test case

---
+++ VehicleRentalTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VehicleRentalTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
